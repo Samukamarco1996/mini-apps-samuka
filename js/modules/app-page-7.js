0a05f03dac1bea30064ba8118ed9269b8aa6cd1e
Samukamarco1996 <samuel.marco1996@gmail.com>
2024-11-06 18:04:00 -0300

    Commit 20241106-1810

--- js/modules/app-page-7.js
+++ js/modules/app-page-7.js
@@ -1,8 +1,9 @@
 class App7 {
-  constructor(inputNewTask, buttonAddTask, divTasksList) {
+  constructor(inputNewTask, buttonAddTask, divTasksList, paragraphMaxChars) {
     this.inputNewTask = inputNewTask;
     this.buttonAddTask = buttonAddTask;
     this.divTasksList = divTasksList;
+    this.paragraphMaxChars = paragraphMaxChars;
   }
 
   taskIdentifier = 0;
@@ -18,6 +19,11 @@ class App7 {
       "beforeunload",
       (event) => (event.returnValue = "Tem certeza que deseja sair da página?")
     );
+    this.eventsListener(
+      this.inputNewTask,
+      "input",
+      this.debounceFunct(this.checkMaxChars.bind(this), 1000)
+    );
     this.eventsListener(this.inputNewTask, "keypress", (event) => {
       if (event.key === "Enter") {
         this.addNewTask();
@@ -29,48 +35,62 @@ class App7 {
     element.addEventListener(eventType, callback);
   }
 
+  debounceFunct(func, delay) {
+    let timer;
+    return function (...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  }
+
   addNewTask() {
-    if (this.inputNewTask.value.trim() !== "") {
-      const newTask = window.document.createElement("div");
-      const inputCheckbox = window.document.createElement("input");
-      const labelDescription = window.document.createElement("label");
-      const buttonDelete = window.document.createElement("button");
-      const imgDeleteIcon = window.document.createElement("img");
+    if (this.inputNewTask.value.trim().length <= 200) {
+      if (this.inputNewTask.value.trim() !== "") {
+        const newTask = window.document.createElement("div");
+        const inputCheckbox = window.document.createElement("input");
+        const labelDescription = window.document.createElement("label");
+        const buttonDelete = window.document.createElement("button");
+        const imgDeleteIcon = window.document.createElement("img");
 
-      this.divTasksList.appendChild(newTask);
-      newTask.appendChild(inputCheckbox);
-      newTask.appendChild(labelDescription);
-      newTask.appendChild(buttonDelete);
-      buttonDelete.appendChild(imgDeleteIcon);
+        this.divTasksList.appendChild(newTask);
+        newTask.appendChild(inputCheckbox);
+        newTask.appendChild(labelDescription);
+        newTask.appendChild(buttonDelete);
+        buttonDelete.appendChild(imgDeleteIcon);
 
-      newTask.classList.add("new-task");
-      newTask.setAttribute("id", `divNewTaskId-0${this.taskIdentifier}`);
-      inputCheckbox.setAttribute("type", "checkbox");
-      inputCheckbox.setAttribute("id", `checkboxId-0${this.taskIdentifier}`);
-      labelDescription.setAttribute(
-        "for",
-        `checkboxId-0${this.taskIdentifier}`
-      );
+        newTask.classList.add("new-task");
+        newTask.setAttribute("id", `divNewTaskId-0${this.taskIdentifier}`);
+        inputCheckbox.setAttribute("type", "checkbox");
+        inputCheckbox.setAttribute("id", `checkboxId-0${this.taskIdentifier}`);
+        labelDescription.setAttribute(
+          "for",
+          `checkboxId-0${this.taskIdentifier}`
+        );
 
-      this.addDeleteAction(buttonDelete, newTask);
+        this.addDeleteAction(buttonDelete, newTask);
 
-      this.taskIdentifier++;
+        this.taskIdentifier++;
 
-      labelDescription.innerText = this.inputNewTask.value;
+        labelDescription.innerText = this.inputNewTask.value;
 
-      imgDeleteIcon.setAttribute(
-        "src",
-        "../imgs/apps-imgs/app-page-7-img-001.png"
-      );
-      imgDeleteIcon.setAttribute("alt", "delete-icon");
+        imgDeleteIcon.setAttribute(
+          "src",
+          "../imgs/apps-imgs/app-page-7-img-001.png"
+        );
+        imgDeleteIcon.setAttribute("alt", "delete-icon");
 
-      this.inputNewTask.value = "";
-      this.inputNewTask.focus();
+        this.inputNewTask.value = "";
+        this.inputNewTask.focus();
+      } else {
+        window.alert(
+          "Digite a descrição da tarefa antes de adicioná-la a lista!"
+        );
+        this.inputNewTask.focus();
+      }
     } else {
-      window.alert(
-        "Digite a descrição da tarefa antes de adicioná-la a lista!"
-      );
-      this.inputNewTask.focus();
+      window.alert("O limite máximo é de até 200 caracteres!");
     }
   }
 
@@ -85,11 +105,16 @@ class App7 {
     taskElement.querySelector("button").removeEventListener("click", listener);
     taskElement.remove();
   }
+
+  checkMaxChars() {
+    this.paragraphMaxChars.innerText = `${this.inputNewTask.value.length}/200 caracteres`;
+  }
 }
 
 const app7Exe = new App7(
   window.document.querySelector("#app-div > div:nth-of-type(2) > input"),
   window.document.querySelector("#app-div > div:nth-of-type(2) > button"),
-  window.document.querySelector("#tasks-list")
+  window.document.querySelector("#tasks-list"),
+  window.document.querySelector("#app-div > div:nth-of-type(2) > p")
 );
 app7Exe.exec();
